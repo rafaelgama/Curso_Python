866459579b7370eab4835fed3037aaa78113db80
Rafael Gama <juniormacedo@gmail.com>
2020-05-10 00:02:48 -0300

    Anotações Aula 20
    Anotações da aula 20

---
+++ Mundo 3/Aulas/aula20.py
@@ -0,0 +1,5 @@
+# Nessa aula, vamos aprender o que são funções ou rotinas e como utilizar funções em Python. 
+# Funções são trechos de código que podem ser executados em momentos diferentes de nossos códigos em Python. 
+# Veja como funciona o comando def em Python e como utilizá-lo com parâmetros simples e múltiplos.
+
+
