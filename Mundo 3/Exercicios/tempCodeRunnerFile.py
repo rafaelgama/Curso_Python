96477feb75021feab86e4bb9c46462d3e52d422b
Rafael Gama <juniormacedo@gmail.com>
2020-05-13 19:12:09 -0300

    Delete do temp

--- Mundo 3/Exercicios/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-cores =  {'limpa':'\033[m',
-            'bverde':'\033[1;32m',
-            'bvermelho': '\033[1;31m',
-            'pretoebranco':'\033[7:30m'}
-
-print('-=-'*10)
-print(cores['pretoebranco']+'_____INICIO_____'+cores['limpa'])
-print('-=-'*10)
