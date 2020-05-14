ac344df07de1808ca8a8a04d6d032861522b3c5c
Rafael Gama <juniormacedo@gmail.com>
2020-05-14 00:44:24 -0300

    Udemy Aula 02
    aula 02 udemy

---
+++ Udemy/Aula02/aula02.py
@@ -0,0 +1,17 @@
+# o comando print
+
+print(123346)
+print('Rafael','Gama', 'Macedo')
+
+# Exemplo com sep
+print('Rafael','Gama', sep='-')
+print('João','e','Maria', sep='-')
+
+
+# Exemplo com end
+print('Rafael','Gama', end='####')
+print('João','e','Maria', end='')
+
+print(f'{"Mascara de CPF":=^40}')
+print('288', '122', '038', sep='.',end='-')
+print('09')
