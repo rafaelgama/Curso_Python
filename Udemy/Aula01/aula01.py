0c32e24e547812c051267b0bbc2b924f3d7c85cb
Rafael Gama <juniormacedo@gmail.com>
2020-05-13 22:09:02 -0300

    Aula 01
    como exibir comentários

---
+++ Udemy/Aula01/aula01.py
@@ -0,0 +1,12 @@
+# comentários
+
+print('Linha 1')
+print('Linha 2')
+# comentário para exemplificar.
+print('Linha 3')
+print('Linha 4')  # Outro comentário
+"""
+print('Linha 5')
+print('Linha 6')
+"""
+# final
