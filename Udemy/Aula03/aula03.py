f9dd4ecf0cf5bae2674e655f96acd75cfb858895
Rafael Gama <juniormacedo@gmail.com>
2020-05-21 16:30:40 -0300

    Ajuste Ambiente Udemy
    Criação do venv na pasta da Udemy

---
+++ Udemy/Aula03/aula03.py
@@ -0,0 +1,13 @@
+'''
+str- string
+'''
+
+print('Essa é uma "string" (str)')
+print("Essa é uma 'string' (str)")
+# sar um caracter de escape para forçar a Aspas
+print('Esse é meu \'TEXTO\' (str).')
+print("Esse é meu \"TEXTO\" (str).")
+
+#Utilizando o r antes do  texto, faz o print ignorar os comando dentro da expressão
+# exemplo com o \n que seria uma quebra de linha
+print(r'Esse é meu \n texto (str)')
